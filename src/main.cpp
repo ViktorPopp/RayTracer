516c09737e190775a70a463f1990179261389ce4
Viktor Popp <viktorpopp.dev@gmail.com>
2025-06-10 16:43:46 +0200

    Formatted `src/main.cpp`

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,12 @@ int main()
     int image_height = 256;
 
     // Render
-    std::cout
-        << "P3\n"
-        << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int i = 0; i < image_height; i++)
-    {
+    for (int i = 0; i < image_height; i++) {
         std::clog << "\rScanlines remaining: " << (image_height - i) << ' ' << std::flush;
 
-        for (int j = 0; j < image_width; j++)
-        {
+        for (int j = 0; j < image_width; j++) {
             auto r = double(j) / (image_width - 1);
             auto g = double(i) / (image_height - 1);
             auto b = 0.0;
