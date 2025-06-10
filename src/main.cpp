96918ee94b0333e109b953a548f04b9638b067dc
Viktor Popp <viktorpopp.dev@gmail.com>
2025-06-10 17:27:18 +0200

    Added a shaded sphere based off normals

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,26 @@
 #include "Ray.hpp"
 #include "Vector3.hpp"
 
-bool hit_sphere(const Point3& center, double radius, const Ray& r) {
+double hit_sphere(const Point3& center, double radius, const Ray& r) {
     Vector3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b * b - 4 * a * c;
-    return (discriminant >= 0);
+    
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - std::sqrt(discriminant) ) / (2.0 * a);
+    }
 }
 
 Color ray_color(const Ray& r) {
-    if (hit_sphere(Point3(0, 0, -1), 0.5, r))
-        return Color(1, 0, 0);
+    auto t = hit_sphere(Point3(0,0, -1), 0.5, r);
+    if (t > 0.0) {
+        Vector3 N = unit_vector(r.at(t) - Vector3(0,0,-1));
+        return 0.5 * Color(N.x + 1, N.y + 1, N.z + 1);
+    }
     Vector3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y + 1.0);
     return (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);
